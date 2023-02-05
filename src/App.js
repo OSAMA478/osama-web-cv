debee7c83656330b8e9e9b217994b8dc6f349004
OSAMA SHAIKH <shaikhosama478@gmail.com>
2023-02-05 10:45:26 +0530

    app is under construction

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const App = () => {
 	return (
 		<main className="flex items-center justify-center h-screen">
 			<p className="p-2 text-2xl capitalize bg-red-300">
-				use this template for react practice with tailwind
+				portfolio is under construction...
 				<SiTailwindcss className="inline mx-1 text-cyan-500" />
 			</p>
 		</main>
