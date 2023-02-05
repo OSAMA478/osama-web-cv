cbab18ea4f3c75848d4415eac879e452efa6868a
OSAMA SHAIKH <shaikhosama478@gmail.com>
2023-02-05 17:55:52 +0530

    ✅ Add: route with initial pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import SideBar from "./Layout/SideBar";
+import Project from "./pages/Project";
+import Testimonials from "./pages/Testimonials";
+import About from "./pages/About";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Skills from "./pages/Skills";
+import NoPage from "./pages/NoPage";
+import Header from "./Layout/Header";
 
 const App = () => {
 	return (
-		<main className="h-screen mx-auto my-0 bg-slate-700 max-w-7xl">
-			<SideBar />
+		<main className="relative h-screen mx-auto my-0 bg-slate-700 max-w-7xl">
+			<Header />
+			<div className="grid grid-cols-[auto_1fr]">
+				<Routes>
+					<Route path="/" element={<SideBar />}>
+						<Route index element={<Home />} />
+						<Route path="project" element={<Project />} />
+						<Route path="skills" element={<Skills />} />
+						<Route path="testimonials" element={<Testimonials />} />
+						<Route path="about" element={<About />} />
+						<Route path="*" element={<NoPage />} />
+					</Route>
+				</Routes>
+			</div>
 		</main>
 	);
 };
--- src/Layout/SideBar.js
+++ src/Layout/SideBar.js
@@ -4,90 +4,89 @@ import { BsPersonSquare } from "react-icons/bs";
 import { GiSkills } from "react-icons/gi";
 import { AiOutlineFundProjectionScreen } from "react-icons/ai";
 import { RiMedalLine } from "react-icons/ri";
+import { BsInfoSquare } from "react-icons/bs";
+import { Link, NavLink, Outlet } from "react-router-dom";
 
 const SideBar = () => {
 	return (
-		<div className="relative ">
-			<nav className="absolute top-0 left-0 z-50 w-full shadow-lg bg-slate-700 ">
-				<div className="px-3 py-3 lg:px-5 lg:pl-3">
-					<div className="flex items-center justify-between text-gray-300">
-						<button
-							data-drawer-target="logo-sidebar"
-							data-drawer-toggle="logo-sidebar"
-							aria-controls="logo-sidebar"
-							type="button"
-							className="inline-flex items-center p-2 text-sm rounded-lg md:hidden hover:bg-slate-500 focus:outline-none focus:ring-2 focus:ring-gray-200 "
-						>
-							<span className="sr-only">Open sidebar</span>
-							<svg
-								className="w-6 h-6"
-								aria-hidden="true"
-								fill="currentColor"
-								viewBox="0 0 20 20"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path
-									clip-rule="evenodd"
-									fill-rule="evenodd"
-									d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
-								></path>
-							</svg>
-						</button>
-						<h1 className="sm:text-2xl">Osama CV</h1>
-						<VscFilePdf className="w-6 h-6 sm:w-8 sm:h-8 " />
-					</div>
-				</div>
-			</nav>
-
+		<>
 			<aside
 				id="logo-sidebar"
-				className="absolute top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full shadow-lg sm:translate-x-0 bg-slate-700"
+				className="z-40 pt-16 w-64 h-[calc(100vh)] transition duration-200 -translate-x-full shadow-lg sm:block sm:translate-x-0 bg-slate-700"
 				aria-label="Sidebar"
 			>
-				<div className="h-full px-3 pb-4 overflow-y-auto bg-slate-700">
-					<ul className="space-y-2">
+				<div className="h-full p-2 sm:p-4 bg-slate-700">
+					<ul className="space-y-2 ">
 						<li>
-							<a
+							<NavLink
+								style={({ isActive }) => ({
+									backgroundColor: isActive ? "rgb(81 102 133)" : "transparent",
+								})}
+								to="/"
 								href="#"
 								className="flex items-center p-2 text-base font-normal text-gray-300 transition duration-100 rounded-lg active:bg-teal-500 dark:text-white hover:bg-slate-500 dark:hover:bg-gray-700"
 							>
 								<BsPersonSquare className="w-4 h-4 sm:w-6 sm:h-6 " />
-								<span className="ml-3">About</span>
-							</a>
+								<span className="ml-3">Home</span>
+							</NavLink>
 						</li>
 						<li>
-							<a
-								href="#"
+							<NavLink
+								style={({ isActive }) => ({
+									backgroundColor: isActive ? "rgb(81 102 133)" : "transparent",
+								})}
+								to="/skills"
 								className="flex items-center p-2 text-base font-normal text-gray-300 transition duration-100 rounded-lg active:bg-teal-500 dark:text-white hover:bg-slate-500 dark:hover:bg-gray-700"
 							>
 								<GiSkills className="w-4 h-4 sm:w-6 sm:h-6 " />
 								<span className="flex-1 ml-3 whitespace-nowrap">Skills</span>
-							</a>
+							</NavLink>
 						</li>
 						<li>
-							<a
-								href="#"
+							<NavLink
+								style={({ isActive }) => ({
+									backgroundColor: isActive ? "rgb(81 102 133)" : "transparent",
+								})}
+								to="/project"
 								className="flex items-center p-2 text-base font-normal text-gray-300 transition duration-100 rounded-lg active:bg-teal-500 dark:text-white hover:bg-slate-500 dark:hover:bg-gray-700"
 							>
 								<AiOutlineFundProjectionScreen className="w-4 h-4 sm:w-6 sm:h-6 " />
 								<span className="flex-1 ml-3 whitespace-nowrap">Project</span>
-							</a>
+							</NavLink>
 						</li>
 						<li>
-							<a
-								href="#"
+							<NavLink
+								style={({ isActive }) => ({
+									backgroundColor: isActive ? "rgb(81 102 133)" : "transparent",
+								})}
+								to="/testimonials"
 								className="flex items-center p-2 text-base font-normal text-gray-300 transition duration-100 rounded-lg active:bg-teal-500 dark:text-white hover:bg-slate-500 dark:hover:bg-gray-700"
 							>
 								<RiMedalLine className="w-4 h-4 sm:w-6 sm:h-6 " />
 								<span className="flex-1 ml-3 whitespace-nowrap">
-									Testimonals
+									Testimonials
 								</span>
-							</a>
+							</NavLink>
+						</li>
+						<li>
+							<NavLink
+								style={({ isActive }) => ({
+									backgroundColor: isActive ? "rgb(81 102 133)" : "transparent",
+								})}
+								to="/about"
+								className="flex items-center p-2 text-base font-normal text-gray-300 transition duration-100 rounded-lg active:bg-teal-500 dark:text-white hover:bg-slate-500 dark:hover:bg-gray-700"
+							>
+								<BsInfoSquare className="w-4 h-4 sm:w-6 sm:h-6 " />
+								<span className="flex-1 ml-3 whitespace-nowrap">About</span>
+							</NavLink>
 						</li>
 					</ul>
 				</div>
 			</aside>
-		</div>
+			<div className="col-start-1 col-end-3 row-start-1 row-end-2 pt-16 sm:col-end-3 sm:col-start-2 justify-self-center">
+				<Outlet />
+			</div>
+		</>
 	);
 };
 
